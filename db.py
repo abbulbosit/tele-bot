44b87f228c8d4953e8b26b72bf684a16e4610a96
Abdulbosit <abdulbosit@gmail.com>
2024-06-17 02:15:49 +0500

    bu mening ishim

---
+++ db.py
@@ -0,0 +1,33 @@
+import psycopg2 as psql
+import os
+
+class Database:
+    @staticmethod
+    async def connect(query, query_type):
+        db = psql.connect(
+            database="users",
+            user="postgres",
+            password="2009",
+            host="localhost",
+            port="5432"
+        )
+
+        cursor = db.cursor()
+        data = ["insert", "delete"]
+        cursor.execute(query)
+        if query_type in data:
+            db.commit()
+            if query_type == "insert":
+                return "inserted successfully"
+
+        else:
+            return cursor.fetchall()
+
+    @staticmethod
+    async def check_user_id(user_id: int):
+        query = f"SELECT * FROM users_2 WHERE user_id = {user_id}"
+        check_user = await Database.connect(query, query_type="select")
+        if len(check_user) == 1:
+            print(">>>>>>>>>>>>>>>>", check_user)
+            return True
+        return False
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import logging
+from db import Database
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = "7450181024:AAHK4U9bJ5Pcbib3GoluxgHdo2uu87ieL08"
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    ful_name = message.from_user.full_name
+    user_id = message.from_user.id
+    username = message.from_user.username
+    query = f"INSERT INTO users_2 (username, full_name, user_id) VALUES ('{username}', '{ful_name}', {user_id})"
+    if await Database.check_user_id(user_id):
+        await message.reply(f"Assalomu aleykum sizni ko'rganimdan xursantman  {ful_name}")
+
+    else:
+        await Database.connect(query, "insert")
+        await message.reply(f"Xushkelibsiz {ful_name}")
+
+
+@dp.message_handler()
+async def talk(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
